ce10638a7f5205373a56a584317e8f03d62b65ec
wada <wadayugo@keio.jp>
2022-08-29 19:43:19 +0900

    get token from env variable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+fn get_github_personal_access_token() -> String {
+    // get token from environment variable
+    let token = std::env::var("GITHUB_PERSONAL_ACCESS_TOKEN").unwrap();
+    token
+}
+
 fn main() {
-    println!("Hello, world!");
+    let token = get_github_personal_access_token();
+    println!("{}", token);
 }
