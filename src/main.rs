2c75e52ec48cf80e932a038697d53a445555d9bd
wada <wadayugo@keio.jp>
2022-09-18 21:19:02 +0900

    get issues

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use serde::{Deserialize, Serialize};
 struct Issue {
     id: i32,
     title: String,
-    body: String,
+    url: String,
 }
 
 fn get_github_personal_access_token() -> String {
@@ -13,39 +13,7 @@ fn get_github_personal_access_token() -> String {
     token
 }
 
-// async fn get_my_issues() -> Vec<Issue> {
-//     let token = get_github_personal_access_token();
-//     let client = reqwest::Client::new();
-//     let res = client
-//         .get("https://api.github.com/issues")
-//         .header("User-Agent", "reqwest")
-//         .header("Accept", "application/vnd.github+json")
-//         .header("Authorization", format!("Bearer {}", token))
-//         .send()
-//         .await;
-//     // if res is Ok, print "OK", otherwise print "Error"
-//     let issues = match res {
-//         Ok(res) => {
-//             println!("Fetch Issues OK");
-//             let parsed_issues = res.json::<Vec<Issue>>().await;
-//             match parsed_issues {
-//                 Ok(issues) => issues,
-//                 Err(e) => {
-//                     println!("Parse Issues Error: {}", e);
-//                     Vec::new()
-//                 }
-//             }
-//         }
-//         Err(err) => {
-//             println!("Fetch Issues Error: {}", err);
-//             let empty_issues: Vec<Issue> = Vec::new();
-//             empty_issues
-//         }
-//     };
-//     issues
-// }
-
-async fn get_my_issues_text() -> String {
+async fn get_my_issues() -> Vec<Issue> {
     let token = get_github_personal_access_token();
     let client = reqwest::Client::new();
     let res = client
@@ -59,18 +27,19 @@ async fn get_my_issues_text() -> String {
     let issues = match res {
         Ok(res) => {
             println!("Fetch Issues OK");
-            let text = res.text().await;
-            match text {
-                Ok(text) => text,
+            let parsed_issues = res.json::<Vec<Issue>>().await;
+            match parsed_issues {
+                Ok(issues) => issues,
                 Err(e) => {
                     println!("Parse Issues Error: {}", e);
-                    String::new()
+                    Vec::new()
                 }
             }
         }
         Err(err) => {
             println!("Fetch Issues Error: {}", err);
-            String::new()
+            let empty_issues: Vec<Issue> = Vec::new();
+            empty_issues
         }
     };
     issues
@@ -78,12 +47,9 @@ async fn get_my_issues_text() -> String {
 
 #[tokio::main]
 async fn main() {
-    // let my_issues = get_my_issues().await;
-    // // print issues
-    // for issue in my_issues {
-    //     println!("{:?}", issue);
-    // }
-    let my_issues_text = get_my_issues_text().await;
-    // print my issues text
-    println!("{}", my_issues_text);
+    let my_issues = get_my_issues().await;
+    // print issues
+    for issue in my_issues {
+        println!("{:?}", issue);
+    }
 }
