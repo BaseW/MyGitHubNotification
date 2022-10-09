187b9c5bc15047535db848435c33d842c557a07c
wada <wadayugo@keio.jp>
2022-10-09 20:47:36 +0900

    add command for editing .env

---
+++ bin/edit-env.sh
@@ -0,0 +1,33 @@
+S3_BUCKET_NAME="alpaca-env-files"
+TMP_DIR=tmp
+
+# if TMP_DIR does not exist, create it
+if [ ! -d "$TMP_DIR" ]; then
+  mkdir $TMP_DIR
+fi
+
+# cp .env from s3
+aws s3 cp s3://$S3_BUCKET_NAME/.env $TMP_DIR/.env.bak
+# cp .env.bak as .env
+cp $TMP_DIR/.env.bak $TMP_DIR/.env
+
+# open .env by vim
+vim $TMP_DIR/.env
+
+# check diff
+diff=`diff $TMP_DIR/.env.bak $TMP_DIR/.env`
+
+# echo diff, and check dialog
+if [ "$diff" != "" ]; then
+  echo "diff:"
+  echo $diff
+  echo "Do you want to upload .env to s3? [y/N]"
+  read answer
+  if [ "$answer" = "y" ]; then
+    aws s3 cp $TMP_DIR/.env s3://$S3_BUCKET_NAME/.env
+  else
+    echo "Canceled."
+  fi
+else
+  echo "No diff."
+fi
