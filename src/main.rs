68bec6d2100e10a54a6562f4e6b1d1dd0bc585d0
wada <wadayugo@keio.jp>
2022-11-06 18:02:47 +0900

    add tests for github.rs

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,8 @@ async fn main() {
     let token = get_github_personal_access_token();
     let webhook_url = get_slack_webhook_url_from_env();
 
-    let my_issues = get_my_issues(token).await;
+    let github_api_addr = "https://api.github.com".to_string();
+    let my_issues = get_my_issues(github_api_addr, token).await;
     let sorted_issues = sort_issues(my_issues);
     let payload = create_payload_for_slack(sorted_issues);
 
