be661e8bc2f3ff12d674ba3aa4321931af59b334
wada <wadayugo@keio.jp>
2023-02-18 12:40:44 +0900

    add function that validate request

--- github-notification/src/env.rs
+++ github-notification/src/env.rs
@@ -1,11 +1,20 @@
+pub const GITHUB_PERSONAL_ACCESS_TOKEN_KEY: &str = "GITHUB_PERSONAL_ACCESS_TOKEN";
+pub const SLACK_WEBHOOK_URL_KEY: &str = "SLACK_WEBHOOK_URL";
+pub const SLACK_SLASH_COMMAND_TOKEN_KEY: &str = "SLACK_SLASH_COMMAND_TOKEN";
+
 pub fn get_github_personal_access_token() -> String {
     // get token from environment variable
-    std::env::var("GITHUB_PERSONAL_ACCESS_TOKEN").unwrap()
+    std::env::var(GITHUB_PERSONAL_ACCESS_TOKEN_KEY).unwrap()
 }
 
 pub fn get_slack_webhook_url_from_env() -> String {
     // get token from environment variable
-    std::env::var("SLACK_WEBHOOK_URL").unwrap()
+    std::env::var(SLACK_WEBHOOK_URL_KEY).unwrap()
+}
+
+pub fn get_slack_slash_command_token() -> String {
+    // get token from environment variable
+    std::env::var(SLACK_SLASH_COMMAND_TOKEN_KEY).unwrap()
 }
 
 #[cfg(test)]
@@ -14,31 +23,49 @@ mod tests {
 
     #[test]
     fn test_get_github_personal_access_token_1() {
-        std::env::set_var("GITHUB_PERSONAL_ACCESS_TOKEN", "test_token");
+        let test_token = "test_token";
+        std::env::set_var(GITHUB_PERSONAL_ACCESS_TOKEN_KEY, test_token);
         let token = get_github_personal_access_token();
-        assert_eq!(token, "test_token");
+        assert_eq!(token, test_token);
     }
 
     #[test]
     #[should_panic]
     fn test_get_github_personal_access_token_2() {
         // reset environment variable
-        std::env::remove_var("GITHUB_PERSONAL_ACCESS_TOKEN");
+        std::env::remove_var(GITHUB_PERSONAL_ACCESS_TOKEN_KEY);
         get_github_personal_access_token();
     }
 
     #[test]
     fn test_get_slack_webhook_url_from_env_1() {
-        std::env::set_var("SLACK_WEBHOOK_URL", "test_url");
+        let test_url = "test_url";
+        std::env::set_var(SLACK_WEBHOOK_URL_KEY, test_url);
         let url = get_slack_webhook_url_from_env();
-        assert_eq!(url, "test_url");
+        assert_eq!(url, test_url);
     }
 
     #[test]
     #[should_panic]
     fn test_get_slack_webhook_url_from_env_2() {
         // reset environment variable
-        std::env::remove_var("SLACK_WEBHOOK_URL");
+        std::env::remove_var(SLACK_WEBHOOK_URL_KEY);
         get_slack_webhook_url_from_env();
+    }
+
+    #[test]
+    fn test_get_slack_slash_command_token_1() {
+        let test_token = "test_token";
+        std::env::set_var(SLACK_SLASH_COMMAND_TOKEN_KEY, test_token);
+        let token = get_slack_slash_command_token();
+        assert_eq!(token, test_token)
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_get_slack_slash_command_token_2() {
+        // reset environment variable
+        std::env::remove_var(SLACK_SLASH_COMMAND_TOKEN_KEY);
+        get_slack_slash_command_token();
     }
 }
--- github-notification/src/slack/mod.rs
+++ github-notification/src/slack/mod.rs
@@ -1,3 +1,4 @@
 pub mod message;
 pub mod notification;
 pub mod payload;
+pub mod slash;
