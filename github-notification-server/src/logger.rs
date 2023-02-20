584bc054830b17665458aec5e55b8995ac3da925
wada <wadayugo@keio.jp>
2023-02-21 00:43:28 +0900

    update log message

--- github-notification-server/src/logger.rs
+++ github-notification-server/src/logger.rs
@@ -1,9 +1,9 @@
 use axum::{http::{StatusCode, Request}, response::Response, middleware::Next};
 
-pub async fn access_log_on_request<B>(
+pub async fn access_log_on_request<B: std::fmt::Debug>(
   req: Request<B>,
   next: Next<B>,
 ) -> Result<Response, StatusCode> {
-  println!("{} {}", req.method(), req.uri());
+  println!("{} {} {:?} {:?}", req.method(), req.uri(), req.headers(), req.body());
   Ok(next.run(req).await)
 }
