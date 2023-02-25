3c2b158bc5bff28e5a0cb88717e6260388de1d9a
wada <wadayugo@keio.jp>
2023-02-25 14:26:43 +0900

    update crate

--- github-notification-server/src/main.rs
+++ github-notification-server/src/main.rs
@@ -6,9 +6,9 @@ use github_notification::sentry::initialize_sentry;
 use github_notification_server::handlers::{
     health_check::health_check_handler, notification::create_notification_handler,
 };
-use github_notification_server::logger::access_log_on_request;
 use std::net::SocketAddr;
 use tower::ServiceBuilder;
+use tower_http::trace::TraceLayer;
 
 #[tokio::main]
 async fn main() {
@@ -18,10 +18,7 @@ async fn main() {
     let app = Router::new()
         .route("/", get(health_check_handler))
         .route("/create-notification", post(create_notification_handler))
-        .layer(
-            ServiceBuilder::new()
-              .layer(axum::middleware::from_fn(access_log_on_request))
-        );
+        .layer(ServiceBuilder::new().layer(TraceLayer::new_for_http()));
 
     // run it
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
