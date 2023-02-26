e77313d9178679d45d01d37debce63e03f4728cc
wada <wadayugo@keio.jp>
2023-02-26 22:59:26 +0900

    execute `cargo fmt`

--- github-notification-server/src/logger.rs
+++ github-notification-server/src/logger.rs
@@ -1,9 +1,19 @@
-use axum::{http::{StatusCode, Request}, response::Response, middleware::Next};
+use axum::{
+    http::{Request, StatusCode},
+    middleware::Next,
+    response::Response,
+};
 
 pub async fn access_log_on_request<B: std::fmt::Debug>(
-  req: Request<B>,
-  next: Next<B>,
+    req: Request<B>,
+    next: Next<B>,
 ) -> Result<Response, StatusCode> {
-  println!("{} {} {:?} {:?}", req.method(), req.uri(), req.headers(), req.body());
-  Ok(next.run(req).await)
+    println!(
+        "{} {} {:?} {:?}",
+        req.method(),
+        req.uri(),
+        req.headers(),
+        req.body()
+    );
+    Ok(next.run(req).await)
 }
