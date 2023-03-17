4c36064c9769045b29d76b2e22c723c30e987b2a
wada <wada@avita.co.jp>
2023-03-17 14:36:13 +0900

    execute `cargo fmt`

--- github-notification/src/sentry.rs
+++ github-notification/src/sentry.rs
@@ -2,19 +2,19 @@ use sentry::ClientInitGuard;
 
 pub fn initialize_sentry() -> Option<ClientInitGuard> {
     match std::env::var("SENTRY_DSN") {
-      Ok(sentry_dsn) => {
-        let guard = sentry::init((
-            sentry_dsn,
-            sentry::ClientOptions {
-                release: sentry::release_name!(),
-                ..Default::default()
-            },
-        ));
-        Some(guard)
-      },
-      _ => {
-        println!("SENTRY_DSN is not set");
-        None
-      }
+        Ok(sentry_dsn) => {
+            let guard = sentry::init((
+                sentry_dsn,
+                sentry::ClientOptions {
+                    release: sentry::release_name!(),
+                    ..Default::default()
+                },
+            ));
+            Some(guard)
+        }
+        _ => {
+            println!("SENTRY_DSN is not set");
+            None
+        }
     }
 }
