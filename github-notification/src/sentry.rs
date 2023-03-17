b849a028efc416081f371269b015fcffd354f056
wada-avita <102776513+wada-avita@users.noreply.github.com>
2023-03-17 14:57:37 +0900

    modify to enable app functionality without Sentry dependency (#52)
    * modify to enable app functionality without Sentry dependency
    
    * execute `cargo fmt`

--- github-notification/src/sentry.rs
+++ github-notification/src/sentry.rs
@@ -1,12 +1,20 @@
 use sentry::ClientInitGuard;
 
-pub fn initialize_sentry() -> ClientInitGuard {
-    let sentry_dsn = std::env::var("SENTRY_DSN").expect("SENTRY_DSN is not set");
-    sentry::init((
-        sentry_dsn,
-        sentry::ClientOptions {
-            release: sentry::release_name!(),
-            ..Default::default()
-        },
-    ))
+pub fn initialize_sentry() -> Option<ClientInitGuard> {
+    match std::env::var("SENTRY_DSN") {
+        Ok(sentry_dsn) => {
+            let guard = sentry::init((
+                sentry_dsn,
+                sentry::ClientOptions {
+                    release: sentry::release_name!(),
+                    ..Default::default()
+                },
+            ));
+            Some(guard)
+        }
+        _ => {
+            println!("SENTRY_DSN is not set");
+            None
+        }
+    }
 }
